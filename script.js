8e0acfd67234827e78e99042240a313cd1945a9b
MohdShadab9887 <mohammedshadab9887@gmail.com>
2024-06-22 12:22:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function convertSecondsToMinutes(totalSeconds) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`http://127.0.0.1:5500/${currFolder}/`);
+  let a = await fetch(`/song/${currFolder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
