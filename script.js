88db08cf8a0a3db1624fede13fc1709c4f8c94bf
MohdShadab9887 <mohammedshadab9887@gmail.com>
2024-06-22 12:42:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function convertSecondsToMinutes(totalSeconds) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`/song/${currFolder}/`);
+  let a = await fetch(`/${currFolder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
